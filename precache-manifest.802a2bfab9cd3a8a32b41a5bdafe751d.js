8bf27a71fdbb84daac9986db280a839d9244f6a3
Judy <codelinked@gmail.com>
2019-07-18 15:30:49 -0400

    Updates

---
+++ precache-manifest.802a2bfab9cd3a8a32b41a5bdafe751d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73704b596a32f9389fc0f35a74d28331",
+    "url": "/techworks_rps/index.html"
+  },
+  {
+    "revision": "c62b2a9968f3db66ede3",
+    "url": "/techworks_rps/static/css/main.93530f77.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/techworks_rps/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "c62b2a9968f3db66ede3",
+    "url": "/techworks_rps/static/js/main.3cd441db.chunk.js"
+  },
+  {
+    "revision": "117e1568c3ee13b79dd5",
+    "url": "/techworks_rps/static/js/runtime~main.8937c884.js"
+  }
+]);
